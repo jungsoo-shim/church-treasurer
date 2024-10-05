2d84c8e41e84930b77d35d515ab99646a318e2c1
Jungsoo Shim <jungsoo.shim.dev@gmail.com>
2024-10-05 07:49:17 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Header = ({ isLoggedIn, onLogin }) => {
   return (
-    <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 flex justify-between items-center">
-      <div className="font-poppins text-lg">Church Treasurer</div>
+    <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 p-4 flex justify-between items-center">
+      <div className="font-bold text-lg">Church Treasurer</div>
       {isLoggedIn ? (null) : (
         <div>
           <nav>
-            <ul className="space-x-4 flex justify-stretch">
+            <ul className="space-x-2 md:space-x-16 flex justify-stretch">
               <li><a href="">Home</a></li>
               <li><a href="">About</a></li>
               <li><a href="">Services</a></li>
