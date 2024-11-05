14690c8005ffff42822713d0ec5da98e4bdb75a3
Jungsoo Shim <jungsoo.shim.dev@gmail.com>
2024-11-05 18:35:36 -0500

    add Logo.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Logo from './Logo.jsx';
 
 const Header = ({ isLoggedIn, onLogin }) => {
   return (
     <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 p-4 flex justify-between items-center">
-      <div className="font-bold text-lg">Church Treasurer</div>
+      <div className="font-bold text-lg w-1/3">
+        <Logo />
+      </div>
       {isLoggedIn ? (null) : (
-        <div>
+        <div className="w-1/3">
           <nav>
-            <ul className="space-x-2 md:space-x-16 flex justify-stretch">
+            <ul className="flex flex-row">
               <li><a href="">Home</a></li>
               <li><a href="">About</a></li>
               <li><a href="">Services</a></li>
@@ -18,7 +21,7 @@ const Header = ({ isLoggedIn, onLogin }) => {
         </div>
       )}
       {isLoggedIn ? (null) : (
-      <div className="font-bold"><button>Log in</button></div>
+      <div className="font-bold w-1/3 items-end"><button>Log in</button></div>
       )}
     </header>
   )
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+  <>
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100" aria-label="CT Logo">
+      <rect width="100" height="100" fill="transparent"/>
+      <text x="50%" y="50%" font-size="40" font-family="Poppins, sans-serif" text-anchor="middle" fill="white" dy=".3em">CT</text>
+    </svg>
+  </>
+  )
+}
+export default Logo;
