fcea5079d22db37c1fb8b089d512f9055d36c858
Jungsoo Shim <jungsoo.shim.dev@gmail.com>
2024-11-05 22:02:49 -0500

    Add Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import Logo from './Logo.jsx';
 
-const Header = ({ isLoggedIn, onLogin }) => {
+const Header = () => {
   return (
     <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 p-4 flex justify-between items-center">
       <div className="font-bold text-lg w-1/3">
         <Logo />
       </div>
-      {isLoggedIn ? (null) : (
-        <div className="w-1/3">
-          <nav>
-            <ul className="flex flex-row">
-              <li><a href="">Home</a></li>
-              <li><a href="">About</a></li>
-              <li><a href="">Services</a></li>
-              <li><a href="">Contact</a></li>
-              <li><a href="">Donate</a></li>
-            </ul>
-          </nav>
-        </div>
-      )}
-      {isLoggedIn ? (null) : (
-      <div className="font-bold w-1/3 items-end"><button>Log in</button></div>
-      )}
+      <div className="hidden sm:block w-1/3">
+        <nav>
+          <ul className="flex flex-row justify-evenly">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Donate</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="hidden sm:block font-bold w-1/3 items-end">
+        <button>Log in</button>
+      </div>
+      <div className="sm:hidden">
+        <button>Menu</button>
+      </div>
     </header>
   )
 }
