292b931db0eff1d2120d77cd5a2a5b4e0c6ec200
Jungsoo Shim <jungsoo.shim.dev@gmail.com>
2024-11-08 15:04:26 -0500

    Modify Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Logo from './Logo.jsx';
+import Login from './Login.jsx';
+import Menu from './Menu.jsx';
 
 const Header = () => {
   return (
-    <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 p-4 flex justify-between items-center">
-      <div className="font-bold text-lg w-1/3">
+    <header className="bg-slate-800 text-white fixed top-0 left-0 w-screen h-12 p-4 flex items-center">
+      <div className="flex w-1/3">
         <Logo />
       </div>
-      <div className="hidden sm:block w-1/3">
+      <div className="flex hidden sm:block w-1/3">
         <nav>
-          <ul className="flex flex-row justify-evenly">
+          <ul className="flex justify-end">
             <li><a href="">Home</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Services</a></li>
@@ -18,11 +20,11 @@ const Header = () => {
           </ul>
         </nav>
       </div>
-      <div className="hidden sm:block font-bold w-1/3 items-end">
-        <button>Log in</button>
+      <div className="flex justify-end hidden sm:block w-1/3">
+        <Login />
       </div>
-      <div className="sm:hidden">
-        <button>Menu</button>
+      <div className="flex sm:hidden">
+        <Menu />
       </div>
     </header>
   )
---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <button>Menu</button>
+  )
+}
+
+export default Menu;
